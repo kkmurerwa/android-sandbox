5eeec02b29e86d4b160114c7f531655e7e1611ae
Kenneth Murerwa <xwaxes@gmail.com>
2023-03-15 14:59:05 +0300

    feat: Added plurals and tests on chaining plurals with multiple variables

--- app/src/main/java/com/murerwa/sandboxapplication/MainActivity.java
+++ app/src/main/java/com/murerwa/sandboxapplication/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.textView);
         TextView textView2 = findViewById(R.id.textView2);
+        TextView textView3 = findViewById(R.id.textView3);
 
         String hello = "Hello";
         String world = "World";
@@ -21,8 +22,24 @@ public class MainActivity extends AppCompatActivity {
         textView.setText(
            this.getString(R.string.hello_world, hello, world)
         );
+
+        String chapterCountText = this.getResources().getQuantityString(
+           R.plurals.chapter_count, 3, 3
+        );
+
         textView2.setText(
-           this.getResources().getQuantityString(R.plurals.story_count, 3, "One hundred")
+           this.getString(R.string.chapter_count_with_story_name, chapterCountText, "The Story of the Three Little Pigs")
+        );
+
+        // New string format
+        textView3.setText(
+           this.getResources().getQuantityString(
+              R.plurals.chapter_count_new,
+               1,
+               3,
+               "The Story of the Three Little Pigs",
+               "Something else"
+           )
         );
     }
 }
